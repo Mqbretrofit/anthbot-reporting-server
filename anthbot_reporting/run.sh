9ef3f8f19c1098d09b60fa6d1fe5013518e7a83f
Mqbretrofit <matrai.attila975@gmail.com>
2026-09-09 23:01:49 +0200

    Start reporting server through migration-aware entrypoint

--- anthbot_reporting/run.sh
+++ anthbot_reporting/run.sh
@@ -5,7 +5,7 @@ export ANTHBOT_DB_PATH="/data/anthbot_reporting.sqlite3"
 export ANTHBOT_ADMIN_TOKEN="$(bashio::config 'admin_token')"
 
 bashio::log.info "Starting ANTHBOT Reporting Server on port 8080"
-exec uvicorn asgi:app \
+exec uvicorn entrypoint:app \
   --host 0.0.0.0 \
   --port 8080 \
   --proxy-headers \
