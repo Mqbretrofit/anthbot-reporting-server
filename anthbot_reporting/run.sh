2f9293c7616c92778a655dbdfc8910f4164633c6
Mqbretrofit <matrai.attila975@gmail.com>
2026-09-19 19:58:30 +0200

    Export site analytics toggle

--- anthbot_reporting/run.sh
+++ anthbot_reporting/run.sh
@@ -12,6 +12,7 @@ export ANTHBOT_PRIVACY_CONTROLLER_NAME="$(bashio::config 'privacy_controller_nam
 export ANTHBOT_PRIVACY_CONTROLLER_ADDRESS="$(bashio::config 'privacy_controller_address')"
 export ANTHBOT_PRIVACY_CONTACT_EMAIL="$(bashio::config 'privacy_contact_email')"
 export ANTHBOT_PRIVACY_CONTACT_PHONE="$(bashio::config 'privacy_contact_phone')"
+export ANTHBOT_SITE_ANALYTICS_ENABLED="$(bashio::config 'site_analytics_enabled')"
 
 bashio::log.info "Starting ANTHBOT Reporting Server on port 8080"
 exec uvicorn entrypoint:app \
